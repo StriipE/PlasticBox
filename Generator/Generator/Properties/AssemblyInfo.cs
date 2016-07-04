ae2f9e03a551b29b7dadb64c00c6720b7ebddbd7
Ewen Auffret <auffret.ewen@gmail.com>
2016-07-04 15:21:33 +0200

    Revert "Création des composants de l'application, création des futures classes de Connector & Generator"
    This reverts commit 7e9c92e65f5ca33c0885fb5c2db639eb913cdcfc.

--- Generator/Generator/Properties/AssemblyInfo.cs
+++ Generator/Generator/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // Les informations générales relatives à un assembly dépendent de 
 // l'ensemble d'attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
 // associées à un assembly.
-[assembly: AssemblyTitle("UserInterface")]
+[assembly: AssemblyTitle("Generator")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("UserInterface")]
+[assembly: AssemblyProduct("Generator")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
